34f68faf4d1447c940cb71b59a75662666ab827d
Francis TSAFACK <wilfrancis@yahoo.fr>
2026-08-27 01:27:24 +0200

    add book repository

---
+++ src/Application/Features/Books/Abstracts/IBookRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Domain.Books;
+
+namespace Application.Features.Books.Abstracts;
+
+public interface IBookRepository
+{
+    Task AddAsync(Book book, CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Persistence/BookRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Features.Books.Abstracts;
+using Domain.Books;
+
+namespace Infrastructure.Persistence;
+
+public sealed class BookRepository(ApplicationDbContext context) : IBookRepository
+{
+    public async Task AddAsync(Book book, CancellationToken cancellationToken)
+    {
+        context.Add(book);
+        await context.SaveChangesAsync(cancellationToken);
+    }
+}
